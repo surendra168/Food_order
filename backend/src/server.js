6f7c7a2d22ffbeaa7041a1392a357b7b798ce37b
surendra <reddysurendra294@gmail.com>
2023-10-14 23:03:40 +0530

    removed errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ app.get('*', (req, res) => {
   res.sendFile(indexFilePath);
 })
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
 });
